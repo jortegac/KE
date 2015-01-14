76735691928fc656887dec41db54ce92661a6792
Julian <jortegac@gmail.com>
2015-01-14 12:53:50 +0100

    Correctly read radio buttons. Added location autocomplete. Change some UI elements

--- static/js/main.js
+++ static/js/main.js
@@ -11,14 +11,9 @@ var autocomplete;
 $( document ).ready(function() {
 	
 	// Where the spinner animation will be placed 
-	target = document.getElementById('modal');
-	
+	target = document.getElementById('modal');	
 	initForm();
-	
-	
-	
-	
-
+	initializeLocationAutocomplete();
 });
 
 function initForm() {
@@ -35,11 +30,88 @@ function initForm() {
 }
 
 function submitForm() {
+
+	// Flag for crudely checking if the needed info is in place
+	var flag = true;
+	var errorMessage = "";
+
 	// Get selected disciplines
 	var disciplines = $("#disciplinesDropDown").dropdownCheckbox("checked");
 	
+	// Build disciplines query string
+	var disciplinesQueryString = "";	
+	for (discipline in disciplines){
+		disciplinesQueryString = disciplinesQueryString + "&ds=" + disciplines[discipline].id;
+	}
+	
+	if (disciplinesQueryString == "") {
+		errorMessage = "No disciplines selected. \n";
+		flag = false;
+	}
+	
 	// Get event date
 	var date = $("#date-event").val();
+	
+	// Build date query string
+	var dateQueryString = "";
+	if (date != "") {
+		dateQueryString = "&date=" + date;
+	}
+	
+	// Build location query string
+	var location = $("#locationTextField").val();
+	var locationQueryString = "";	
+	if (location != "" ){
+		locationQueryString = "&location=" + location.substring(0, location.indexOf(","));
+	}else{
+		errorMessage = errorMessage + "No location selected. \n";
+		flag = false;
+	}
+	
+	// Build budget query string
+	var budget = $("#budget").val();
+	var budgetQueryString = "";	
+	if (budget != "" ){
+		budgetQueryString = "&budget=" + budget;
+	}else{
+		errorMessage = errorMessage + "No budget selected. \n";
+		flag = false;
+	}
+	
+	// Build visitors query string
+	var visitors = $("#visitors").val();
+	var visitorsQueryString = "";	
+	if (visitors != "" ){
+		visitorsQueryString = "&visitors=" + visitors;
+	}else{
+		errorMessage = errorMessage + "No visitors selected. \n";
+		flag = false;
+	}
+	
+	// Build skill rating query string
+	//var skill = $("#skill").val();
+	
+	var skill = $('input:radio[name=skill]:checked').val();	
+	var skillQueryString = "&skill=" + skill;	
+	
+	// Build quality rating query string
+	var quality = $('input:radio[name=quality]:checked').val();
+	var qualityQueryString = "&quality=" + quality;	
+	
+	// Build price rating query string
+	var price = $('input:radio[name=price]:checked').val();
+	var priceQueryString = "&price=" + price;	
+	
+	// Check if some data is missing
+	if(!flag){
+		BootstrapDialog.alert({title:"Information", message:errorMessage});
+	}else{
+		// Do stuff
+		
+		// Build full query string
+		var endpoint = "/findSuppliers?" + disciplinesQueryString + dateQueryString + locationQueryString + budgetQueryString + visitorsQueryString + skillQueryString + qualityQueryString + priceQueryString;
+	}
+	
 }
 
 function initDisciplines() {
@@ -85,5 +157,21 @@ function getDisciplines(){
 		});	
 		
 		$("#disciplinesDropDown").dropdownCheckbox("reset", genres);
+	});
+}
+
+function initializeLocationAutocomplete(){
+	// Code for the autocomplete location input text field
+	var input = (document.getElementById('locationTextField'));
+	
+	// Limit the autocompletion to city names of the netherlands
+	var autocompleOptions = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'nl'}
+	};
+	autocomplete = new google.maps.places.Autocomplete(input, autocompleOptions);
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		
+		//reverseGeocodeLocation($("#locationTextField").val());
 	});
 }
